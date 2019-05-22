999659fb16a1ffec603b53e91a21b22b7b9b0f39
Anway Somani <anwaysomani@gmail.com>
2019-05-22 20:53:42 +0530

    Customised User model

--- baltechbook/accounts/models.py
+++ baltechbook/accounts/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from organizations.models import organization
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .constants import *
+
+# Contstants: userType, city, state, country
+class User(AbstractUser):
+    employee_id = models.CharField(max_length=5, null=True)
+    organization = models.CharField(max_length=32, null=True)
+    contactNum = models.CharField(max_length=10, null=True)
+    userType = models.CharField(max_length=15, choices=USERTYPE, null=True)
+    address = models.CharField(max_length=100, null=True)
+    city = models.CharField(max_length=40, null=True)
+    state = models.CharField(max_length=30, choices=STATES, null=True)
+    country = models.CharField(max_length=35, choices=COUNTRY, null=True)
+    pincode = models.CharField(max_length=6, null=True)
+
+
 
