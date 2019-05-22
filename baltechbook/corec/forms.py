da54fdefb9b42e771de3b517ef6706bc8c8474cf
Anway Somani <anwaysomani@gmail.com>
2019-05-22 20:54:43 +0530

    Updated corec for template models

---
+++ baltechbook/corec/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from accounts.models import *
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'first_name', 'last_name', 'email', 'employee_id', 'contactNum', 'address', 'city', 'pincode', 'country']
--- baltechbook/corec/views.py
+++ baltechbook/corec/views.py
@@ -1,12 +1,31 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
+from accounts.models import *
+from .forms import UserForm
 
 def dashboard(request):
     user = request.user
     context = {
-        'user': user,
+        user : 'user',
     }
     return render(request, 'dashboard.html', context)
 
 def invoice(request):
     return render(request, 'invoice.html', {})
+
+def blank(request):
+    form = UserForm()
+    users = User.objects.all()
+    context = {
+        'form': form,
+        'users': users,
+    }
+    
+    if request.method == "POST":
+        form = UserForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserForm()
+    return render(request, 'blank.html', context)
+
