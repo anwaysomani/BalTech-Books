29c4c3f7e851c7e715520c17c7ddbb26b00d1cf7
Anway Somani <anwaysomani@gmail.com>
2019-05-24 16:50:41 +0530

    Cleared models for co-relational subs

--- baltechbook/corec/views.py
+++ baltechbook/corec/views.py
@@ -1,10 +1,11 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from accounts.models import *
-from .forms import UserForm
+from django.contrib.auth import get_user_model
+#from .forms import UserForm
 
 def dashboard(request):
-    user = request.user
+    user = get_user_model()
     context = {
         user : 'user',
     }
@@ -14,18 +15,18 @@ def invoice(request):
     return render(request, 'invoice.html', {})
 
 def blank(request):
-    form = UserForm()
-    users = User.objects.all()
-    context = {
-        'form': form,
-        'users': users,
-    }
+    #form = UserForm()
+    #users = User.objects.all()
+    #context = {
+        #'form': form,
+    #    'users': users,
+    #}
     
-    if request.method == "POST":
-        form = UserForm(request.POST or None)
-        if form.is_valid():
-            form.save()
-    else:
-        form = UserForm()
+    #if request.method == "POST":
+        #form = UserForm(request.POST or None)
+    #    if form.is_valid():
+    #        form.save()
+    #else:
+        #form = UserForm()
     return render(request, 'blank.html', context)
 
