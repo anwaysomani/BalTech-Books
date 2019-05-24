668050e4ba17fa2d61c55b71ad797f621ea9c1f1
Anway Somani <anwaysomani@gmail.com>
2019-05-24 16:52:00 +0530

    Updated model for organization model

--- baltechbook/organizations/models.py
+++ baltechbook/organizations/models.py
@@ -4,21 +4,17 @@ from django.core.validators import RegexValidator
 from PIL import Image
 
 # Model for organization information on pages
-class organization(models.Model):
-    name = models.CharField(max_length=50, primary_key=True)
-    address = models.TextField()
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
-    pincode = models.CharField(max_length=6, validators=[RegexValidator(r'^\d{1,10}$')])
-    country = models.CharField(max_length=75)
-    contactNumber = models.IntegerField()
-    emailAddress = models.EmailField(max_length=200)
-    webLink = models.URLField()
-    gstNo = models.CharField(max_length=15)
-    authorizedGeneral = models.CharField(max_length=100)
-    organizationLogo = models.ImageField()
-    totalOfficials = models.IntegerField()
-
-    def __str__(self):
-        return self.name
+class organization_table(models.Model):
+    organization_id = models.IntegerField(primary_key=True)
+    organization_name = models.CharField(max_length=50)
+    phoneNumber = models.CharField(max_length=10)
+    mobileNumber = models.CharField(max_length=10)
+    whatsappNumber = models.CharField(max_length=10)
+    salesEmail = models.EmailField()
+    infoEmail = models.EmailField(null=True)
+    logo = models.ImageField(null=True)
+    addressLine1 = models.CharField(max_length=200)
+    addressLine2 = models.CharField(max_length=200)
+    city = models.CharField(max_length=30)
+    pincode = models.IntegerField(validators=[RegexValidator(r'^\d{1,10}$')], null=True)
 
