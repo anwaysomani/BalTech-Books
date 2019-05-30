4aafc5ec3905d6986ab3cd94af86f09ec45c2660
Anway Somani <anwaysomani@gmail.com>
2019-05-31 01:03:15 +0530

    Updated stock app with inventory

--- baltechbook/stock/urls.py
+++ baltechbook/stock/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'inventory/$', views.inventory, name='inventory'),
 ]
--- baltechbook/stock/views.py
+++ baltechbook/stock/views.py
@@ -1,6 +1,13 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from stock.models import products_table
 
-# Create your views here.
+def inventory(request):
+    product_list = products_table.objects.all()
+
+    context = {
+        'product_list': product_list,
+    }
+
+    return render(request, 'inventory.html', context)
+
