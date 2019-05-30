59b6436cd43cdc3dfd078ce2e628c678f954b12d
Anway Somani <anwaysomani@gmail.com>
2019-05-31 04:08:35 +0530

    Updated customer_table template

--- baltechbook/customer/views.py
+++ baltechbook/customer/views.py
@@ -4,8 +4,10 @@ from customer.models import customer_table, customer_address_table
 
 
 def customer_record(request):
+    customers = customer_table.objects.all()
     
     context = {
+        'customers': customers,
     }
 
     return render(request, 'customer.html', context)
