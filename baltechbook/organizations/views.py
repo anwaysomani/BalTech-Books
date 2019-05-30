0a123169d60729f123ae926c862d9dd4013ad852
Anway Somani <anwaysomani@gmail.com>
2019-05-30 21:04:51 +0530

    Added team records to project

--- baltechbook/organizations/views.py
+++ baltechbook/organizations/views.py
@@ -1,6 +1,27 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from accounts.models import employee_table
 
-# Create your views here.
+# team member view
+def team(request, id):
+    all_members_list = employee_table.objects.all().order_by('employee_id')
+
+    context = {
+        'all_members': all_members_list,
+        'id': id,
+    }
+
+    return render(request, 'team.html', context)
+
+
+def team_member(request, id, emp_id):
+    member = employee_table.objects.get(employee_id=emp_id)
+
+    context = {
+        'member': member,
+        'id': id,
+    }
+
+    return render(request, 'team-member.html', context)
+
+
