540b447d6251493dc21c25c1035d1fa923ea9e8b
Anway Somani <anwaysomani@gmail.com>
2019-05-30 11:02:29 +0530

    Removed excess from corec forms

--- baltechbook/corec/forms.py
+++ baltechbook/corec/forms.py
@@ -1,7 +1,1 @@
 from django import forms
-from accounts.models import *
-
-#class UserForm(forms.ModelForm):
-#    class Meta:
-#        model = User
-#        fields = ['username', 'password', 'first_name', 'last_name', 'email', 'employee_id', 'contactNum', 'address', 'city', 'pincode', 'country']
