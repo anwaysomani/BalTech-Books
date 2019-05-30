5bb7cfef0009dd1958144e32d216a0af61627001
Anway Somani <anwaysomani@gmail.com>
2019-05-31 01:02:31 +0530

    Views for team template

--- baltechbook/organizations/urls.py
+++ baltechbook/organizations/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'team/(?P<id>\d+)/$', views.team, name='team'),
-    url(r'team/(?P<id>\d+)/member/(?P<emp_id>\d+)/$', views.team_member, name='team-member'),
+    url(r'team/$', views.team, name='team'),
+    url(r'team/member/(?P<emp_id>\d+)/$', views.team_member, name='team-member'),
 ]
--- baltechbook/organizations/views.py
+++ baltechbook/organizations/views.py
@@ -3,23 +3,21 @@ from django.shortcuts import render
 from accounts.models import employee_table
 
 # team member view
-def team(request, id):
+def team(request):
     all_members_list = employee_table.objects.all().order_by('employee_id')
 
     context = {
         'all_members': all_members_list,
-        'id': id,
     }
 
     return render(request, 'team.html', context)
 
 
-def team_member(request, id, emp_id):
+def team_member(request, emp_id):
     member = employee_table.objects.get(employee_id=emp_id)
 
     context = {
         'member': member,
-        'id': id,
     }
 
     return render(request, 'team-member.html', context)
