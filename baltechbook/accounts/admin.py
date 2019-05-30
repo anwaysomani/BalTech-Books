c3301ebb8c7f5f0731c6ccc6369d60f942c93cde
Anway Somani <anwaysomani@gmail.com>
2019-05-30 19:23:57 +0530

    Updated accounts app

--- baltechbook/accounts/admin.py
+++ baltechbook/accounts/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-
 from .forms import UserAdminCreationForm, UserAdminChangeForm
 from .models import employee_table
 
