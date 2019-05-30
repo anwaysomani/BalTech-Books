e1c5e4e5e611f24391220ef775016c2427b476c7
Anway Somani <anwaysomani@gmail.com>
2019-05-31 01:01:12 +0530

    Added customer view template

--- baltechbook/customer/urls.py
+++ baltechbook/customer/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'customer/$', views.customer_record, name='customer-record'),
 ]
--- baltechbook/customer/views.py
+++ baltechbook/customer/views.py
@@ -1,3 +1,12 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
+from customer.models import customer_table, customer_address_table
+
+
+def customer_record(request):
+    
+    context = {
+    }
+
+    return render(request, 'customer.html', context)
 
