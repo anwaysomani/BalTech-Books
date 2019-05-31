d54f966d9cce251676a827fc476b62e2e32defc8
Anway Somani <anwaysomani@gmail.com>
2019-05-31 15:21:16 +0530

    Updated team page with sales exec seperate list

--- baltechbook/organizations/views.py
+++ baltechbook/organizations/views.py
@@ -5,9 +5,11 @@ from accounts.models import employee_table
 # team member view
 def team(request):
     all_members_list = employee_table.objects.all().order_by('employee_id')
+    all_sales_members_list = employee_table.objects.filter(employee_type="Sales Executive")
 
     context = {
         'all_members': all_members_list,
+        'all_sales_members', all_sales_members_list,
     }
 
     return render(request, 'team.html', context)
