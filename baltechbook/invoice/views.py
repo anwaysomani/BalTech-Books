04e466bceeba2d2beb477b34e6bafc9ec62c5f4d
Anway <31155434+anwaysomani@users.noreply.github.com>
2019-06-07 15:59:17 +0530

    Resolved error for null value on invoice gen

--- baltechbook/invoice/views.py
+++ baltechbook/invoice/views.py
@@ -253,7 +253,7 @@ def payment(request, id):
 
     val = 0
     for prod in all_products:
-        val = val + prod.product_price
+        val = val + prod.post_tax_amount
 
     initial_data = {
         'order_id': id,
