ec0ae59f682c8f56d0882434d19c49c6692b6e58
Anway Somani <anwaysomani@gmail.com>
2019-06-14 19:02:06 +0530

    Added daily visit record set

--- baltechbook/customer/urls.py
+++ baltechbook/customer/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'customer/$', views.customer_record, name='customer-record'),
+    url(r'daily-visit-record/$', views.daily_visit_record, name='daily_visit_record'),
 ]
--- baltechbook/customer/views.py
+++ baltechbook/customer/views.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
-from customer.models import customer_table, customer_address_table
+from customer.models import customer_table, customer_address_table, registered_shop
 
 
 def customer_record(request):
@@ -12,3 +12,10 @@ def customer_record(request):
 
     return render(request, 'customer.html', context)
 
+def daily_visit_record(request):
+    shops = registered_shop.objects.all()
+    context = {
+        'shops': shops,
+    }
+
+    return render(request, 'daily_visit_record.html', context)
