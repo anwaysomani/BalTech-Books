9c1c21c884ec0776b5cba5d24fcc39ce728f7390
Anway Somani <anwaysomani@gmail.com>
2019-06-27 16:37:37 +0530

    Updated views for stock interaction

--- baltechbook/stock/views.py
+++ baltechbook/stock/views.py
@@ -4,10 +4,11 @@ from stock.models import products_table
 
 def inventory(request):
     product_list = products_table.objects.all()
-
     context = {
         'product_list': product_list,
     }
 
     return render(request, 'inventory.html', context)
 
+def stockHistory(request):
+    return render(request, 'stockHistory.html', {})
